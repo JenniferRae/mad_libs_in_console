be35859b48967c97b7ae2124de1c244d46807f2b
Heflin <jenniferheflin3@gmail.com>
2015-06-02 20:34:14 -0700

    message recieved, over and out ;)

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 alert("I'm working with JavaScript");
+
+function madLibs() {
+	document.write("Today is Tuesday. Tomorrow is Wednesday. My favorite color is blue. If I could go anywhere in the world, I'd go to Paris. The name of my dog is Max. He is black and tan.")
+}
+
+madLibs();
