83939536061f2a356a72dec39a150d2c1f637cf6
Heflin <jenniferheflin3@gmail.com>
2015-06-02 21:07:27 -0700

    mad libs mission accomplished (I think...at least it runs)

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 alert("I'm working with JavaScript");
 
+
 function madLibs() {
-	document.write("Today is Tuesday. Tomorrow is Wednesday. My favorite color is blue. If I could go anywhere in the world, I'd go to Paris. The name of my dog is Max. He is black and tan.")
+	var weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+	var color = ["black", "yellow", "green", "tan", "blue"];
+	var place = ["Paris", "Canada", "Russia", "China"];
+	var name = ["Bob", "Ken", "George", "Max", "Paul"];
+
+
+	document.write("<h1>Today is " + weekday[1] + ". Tomorrow is " +  weekday[2] + ". My favorite color is " + color[4] + ". If I could go anywhere in the world, I'd go to " + place[0] + ". The name of my dog is " + name[3] + ". He is " + color[0] + " and " + color[3] + ".</h1>")
+
+
 }
 
 madLibs();
+
+
+
+
+
+
